1d53d17694e8b58d7e3f13401b3d8a6f5e0154e6
MishaR <mesharoman12@gmail.com>
2022-08-04 18:28:25 +0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Calculator;
+use App\Http\Livewire\TodoList;
+use App\Http\Livewire\CascadingDropdown;
+use App\Http\Livewire\ProductSearch;
+use App\Http\Livewire\ImageUpload;
+use App\Http\Livewire\RegistrationForm;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/counter', function () {
+    return view('welcome');
+})->name('counter');
+
+Route::get('/calculator', Calculator::class)->name('calculator');
+Route::get('/todo-list', TodoList::class)->name('todo-list');
+Route::get('/cascading-dropdown', CascadingDropdown::class)->name('cascading-dropdown');
+Route::get('/products', ProductSearch::class)->name('products');
+Route::get('/image-upload', ImageUpload::class)->name('image-upload');
+Route::get('/register', RegistrationForm::class)->name('register');
